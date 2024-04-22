8e593048177ff3a33f4d3cab86d5afa810dc9749
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-04-22 09:05:41 +0000

    Task_1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,11 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* eslint-enable no-unused-vars */
+  /* no-unused-vars */
 
   return [task, task2];
 }
