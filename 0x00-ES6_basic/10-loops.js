b9507421a34fb2b9835984a09e94a2149f8d88cc
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-04-22 13:07:25 +0000

    Task_10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,8 +2,8 @@ export default function appendToEachArrayValue(array, appendString) {
     const newArr = [];
   
     for (const val of array) {
-      newArr.push(appendString + val);
+      newArr.push(`${appendString}${val}`);
     }
   
     return newArr;
-  } 
+  }
