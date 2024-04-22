6f5b060f983c56c975387d6744e6f5ae16b088f1
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-04-22 13:15:45 +0000

    Task_11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-    const employeesObject = {};
-    employeesObject[departmentName] = employees;
-    return employeesObject;
-  }
+  return {
+    [`${departmentName}`]: employees,
+  };
+}
