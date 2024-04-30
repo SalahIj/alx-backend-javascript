ddf59db36229e2ce27ac97490eee1740908719cb
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-04-30 08:56:29 +0000

    Task_0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const arr = [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstname: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+
+  return arr;
+}
