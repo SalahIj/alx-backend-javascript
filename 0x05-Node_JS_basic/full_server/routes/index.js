fff84969f8ffede7f22e278aa1e81a0e54727dd4
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-06-18 08:51:19 +0000

    Tasks

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -4,10 +4,8 @@ import { StudentsController } from '../controllers/StudentsController';
 const express = require('express');
 const router = express.Router();
 
-// Route for the homepage
 router.get('/', AppController.getHomepage);
 
-// Route for getting all students or students by major
 router.get('/students/:major?', StudentsController.getAllStudents);
 
 export default router;
