1174d4740b020bf2c15a1d306ad6dfdef70f3672
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-06-18 07:56:16 +0000

    Task_0 & Task_1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * Logging a message to the console.
+ * @param {string} message - The message logged.
+ * @returns {void}
+ */
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exporting the displaying Message.
+ * @module displayMessage
+ * @function
+ */
+module.exports = displayMessage;
